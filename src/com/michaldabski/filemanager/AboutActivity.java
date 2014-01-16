a604e2261d774c948d2f4da21f4a646df10deb60
michaldabski <contact@michaldabski.com>
2014-01-16 20:33:06 +0000

    Add feedback and playstore links to About screen

--- src/com/michaldabski/filemanager/AboutActivity.java
+++ src/com/michaldabski/filemanager/AboutActivity.java
@@ -1,21 +1,28 @@
 package com.michaldabski.filemanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.michaldabski.filemanager.R;
 import com.michaldabski.utils.FontApplicator;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-public class AboutActivity extends Activity
+public class AboutActivity extends Activity implements OnClickListener
 {
 	
+	private static final String PLAYSTORE_URL = "https://play.google.com/store/apps/developer?id=mick88";
+	private static final String FEEDBACK_ADDRESS = "contact@michaldabski.com";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -42,6 +49,9 @@ public class AboutActivity extends Activity
 		}
 		
 		new FontApplicator(getApplicationContext(), "Roboto_Light.ttf").applyFont(getWindow().getDecorView());
+		
+		findViewById(R.id.btnFeedback).setOnClickListener(this);
+		findViewById(R.id.btnPlaystore).setOnClickListener(this);
 	}
 	
 	/**
@@ -72,6 +82,40 @@ public class AboutActivity extends Activity
 				return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+			case R.id.btnFeedback:
+				try
+				{
+					Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+					emailIntent.setData(Uri.parse("mailto:"+FEEDBACK_ADDRESS));
+					emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name)+" feedback");
+					startActivity(emailIntent);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+				break;
+				
+			case R.id.btnPlaystore:
+				try
+				{
+					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(PLAYSTORE_URL));
+					startActivity(intent);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+				break;
+			
+		}
 	}
 	
 }
