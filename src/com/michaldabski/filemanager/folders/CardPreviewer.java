325fe3590838f854b8883d9eb1c76a6b2d8bd849
michaldabski <contact@michaldabski.com>
2014-01-20 22:45:19 +0000

    Generate folder previews
    Add bitmap utils
    
    Respect aspect ratio of images previewed in folders
    
    Add method to calculate thumbnail positions in grid
    
    Calculate item positions in grid
    
    Bigger folder preview
    
    Recycle used thumbnails from building folder preview

--- src/com/michaldabski/filemanager/folders/CardPreviewer.java
+++ src/com/michaldabski/filemanager/folders/CardPreviewer.java
@@ -1,18 +1,30 @@
 package com.michaldabski.filemanager.folders;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
 import com.michaldabski.filemanager.R;
+import com.michaldabski.utils.BitmapUtils;
 import com.michaldabski.utils.FilePreviewCache;
+import com.michaldabski.utils.FileUtils;
 
 public class CardPreviewer extends AsyncTask<File, Void, Bitmap>
 {
 	private static final int THUMBNAIL_SIZE = 256;
+	private static final int NUM_FOLDER_PREVIEWS = 6;
+	private static final int PREVIEW_WIDTH = (int) (THUMBNAIL_SIZE * 1.3),
+			PREVIEW_HEIGHT = THUMBNAIL_SIZE;
 	final ImageView imageView;
 	final FilePreviewCache thumbCache;
 	File file;
@@ -23,9 +35,58 @@ public class CardPreviewer extends AsyncTask<File, Void, Bitmap>
 		this.thumbCache = thumbCache;
 	}
 	
+	Bitmap buildFolderPreview(File folder)
+	{
+		List<Bitmap> previews = new ArrayList<Bitmap>(NUM_FOLDER_PREVIEWS);
+		getBitmapsInFolder(folder, previews);
+		if (previews.isEmpty()) return null;
+		
+		Bitmap folderPreview = Bitmap.createBitmap(PREVIEW_WIDTH, PREVIEW_HEIGHT, Bitmap.Config.ARGB_8888);
+		Rect[] destRect = BitmapUtils.layoutImagesInGrid(folderPreview, 3, 2);
+		Canvas canvas = new Canvas(folderPreview);
+		Paint paint = new Paint();
+		Rect srcRect = new Rect();
+		int i=0;
+		for (Bitmap bitmap : previews)
+		{
+			srcRect = BitmapUtils.getBestFitRect(bitmap, destRect[i]);
+			canvas.drawBitmap(bitmap, srcRect, destRect[i++], paint);
+			bitmap.recycle();
+		}
+		return folderPreview;
+	}
+	
+	void getBitmapsInFolder(File folder, List<Bitmap>previews)
+	{
+		File [] files = folder.listFiles(FileUtils.DEFAULT_FILE_FILTER);
+		Arrays.sort(files, new Comparator<File>()
+		{
+
+			@Override
+			public int compare(File lhs, File rhs)
+			{
+				if (lhs.lastModified() > rhs.lastModified()) return -1;
+				else if (lhs.lastModified() < rhs.lastModified()) return 1;
+				else return 0;
+			}
+			
+		});
+		for (File file : files) 
+			if (file.isDirectory() == false)
+		{
+			Bitmap bitmap = getPreview(file);
+			if (bitmap != null)
+			{
+				previews.add(bitmap);
+				if (previews.size() == NUM_FOLDER_PREVIEWS)
+					break;
+			}
+		}
+	}
+	
 	Bitmap getPreview(File image) 
 	{
-		if (image.isDirectory()) return null;
+		if (image.isDirectory()) return buildFolderPreview(image);
 	    BitmapFactory.Options bounds = new BitmapFactory.Options();
 	    bounds.inJustDecodeBounds = true;
 	    BitmapFactory.decodeFile(image.getPath(), bounds);
---
+++ src/com/michaldabski/utils/BitmapUtils.java
@@ -0,0 +1,55 @@
+package com.michaldabski.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.util.Log;
+
+public class BitmapUtils
+{
+	private static final String LOG_TAG = "BitmapUtils";
+
+	public static Rect getBestFitRect(Bitmap bitmap, Rect destination)
+	{
+		return getBestFitRect(bitmap, (float)destination.width() / (float)destination.height());
+	}
+	
+	public static Rect getBestFitRect(Bitmap bitmap, float ratio)
+	{
+		float bmpRatio = (float)bitmap.getWidth() / (float)bitmap.getHeight();
+			
+		if (bmpRatio > ratio) // bmp is wider
+		{
+			int height = bitmap.getHeight();
+			int width = (int) (height * ratio);
+			int offset = (bitmap.getWidth() - width) / 2;
+			return new Rect(offset, 0, offset+width, height);
+		}
+		else if (bmpRatio < ratio) // bmp is taller
+		{
+			int width = bitmap.getWidth();
+			int height = (int) (width / ratio);
+			int offset = (bitmap.getHeight() - height) / 2;
+			return new Rect(0, offset, width, offset+height);
+		}
+		else return new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+	}
+	
+	public static Rect[] layoutImagesInGrid(Bitmap destination, int cols, int rows)
+	{
+		int numItems = cols*rows;
+		int itemWidth = destination.getWidth() / cols,
+			itemHeight = destination.getHeight() / rows;
+		
+		Rect[] result = new Rect[numItems];
+		
+		for (int i=0; i < numItems; i++)
+		{
+			int x = (i % cols) * itemWidth,
+				y = (i / (rows+1)) * itemHeight;
+			Log.d(LOG_TAG, x+"x"+y);
+			result[i] = new Rect(x, y, x+itemWidth, y+itemHeight);
+		}
+		
+		return result;
+	}
+}
